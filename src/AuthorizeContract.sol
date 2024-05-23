18bb537c52a7111a6bd0182351c6026526e065a8
Tashun Sandaru <tashun.chamara@gmail.com>
2024-05-23 17:22:53 +0530

    buy sell skeleton

--- src/AuthorizeContract.sol
+++ src/AuthorizeContract.sol
@@ -2,16 +2,37 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./TBill.sol";
 
 contract AuthorizeContract is Ownable {
     TBill private tBillContract;
+    ERC20 USDC_Contract = ERC20(address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48));
 
     constructor(address _tBillContract, address owner)
     Ownable(owner)
     {
-        tBillContract = _tBillContract;
+        tBillContract = TBill(_tBillContract);
     }
 
+    function AuthorizeMint(uint256 amount) external onlyOwner {
+        // require() check the vault usd value
+        require(amount % 1==0, "Invalid Amount");
+        tBillContract.mint(address(this), amount);
+    }
+
+    function buy(uint256 amount) external {
+        // require() check the vault usd value
+        require(amount % 1==0, "Invalid Amount");
+        USDC_Contract.transferFrom(msg.sender, address(this), amount);
+        tBillContract.transferFrom(msg.sender, address(this), amount);
+    }
+
+    function sell(uint256 amount) external {
+        // require() check the vault usd value
+        require(amount % 1==0, "Invalid Amount");
+        USDC_Contract.transfer(msg.sender,amount);
+        tBillContract.transfer(msg.sender, amount);
+    }
 
 }
